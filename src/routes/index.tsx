715effd33db4c67ab0ca8a842407f88ca8fb6e8c
IrwanFicoFar <irwanhadi9@gmail.com>
2023-04-04 14:57:21 +0700

    oke

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ import Home from "@/pages";
 import Login from "@/pages/auth/Login";
 import Register from "@/pages/auth/Register";
 import Profile from "@/pages/Profile";
-import { MyNotFound } from "@/pages/MyNotFound";
+import { NotFound } from "@/pages/NotFound";
 
 import { handleAuth } from "@/utils/redux/reducer/reducer";
 import { ThemeContext } from "@/utils/context";
@@ -31,7 +31,7 @@ const Router: FC = () => {
     {
       path: "/",
       element: <Home />,
-      errorElement: <MyNotFound />,
+      errorElement: <NotFound />,
     },
     {
       path: "/login",
